6ef63491e8a6f534e3aad40ee7ef0aad9637b1b0
javedsiddique <javedsiddique943@gmail.com>
2020-05-09 15:03:59 +0530

    Initial Commit

---
+++ XmlParsing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XmlParsing
+//
+//  Created by JAVED SIDDIQUE on 09/05/20.
+//  Copyright © 2020 JAVED SIDDIQUE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
